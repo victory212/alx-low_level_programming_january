818cc043b03c448c95dce23d0318a47ffa0de231
victory212 <okoivictory2000@gmail>
2022-12-12 18:40:31 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
+
+}
