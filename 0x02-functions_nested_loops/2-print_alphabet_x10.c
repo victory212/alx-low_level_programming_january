c6257913e1c1828a68d6cc725f2126f5d0be65a2
victory212 <okoivictory2000@gmail>
2022-12-27 01:25:09 -0800

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	_putchar(la);
+	_putchar('\n');
+}
