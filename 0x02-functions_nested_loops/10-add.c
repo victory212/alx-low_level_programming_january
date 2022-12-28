1683e3942d4413fa95a48f77c80cf89af8ebe221
victory212 <okoivictory2000@gmail>
2022-12-28 01:52:13 -0800

    addition of int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - addition of numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the added result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
