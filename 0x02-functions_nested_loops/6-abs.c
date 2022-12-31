fdbbefe21321bc867336a1d1049c1b5fa4a9399f
victory212 <okoivictory2000@gmail>
2022-12-31 07:31:48 -0800

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the ansolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (0);
+	}
+	return (-n);
+}
